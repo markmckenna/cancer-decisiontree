15e0f8d4d950d5b4b02c5ef24d0ebd3500e914fa
Mark McKenna <mark.mckenna@redspace.com>
2026-02-09 09:53:18 -0400

    base setup

---
+++ src/main.py
@@ -0,0 +1,5 @@
+# Standard starting block in your main script
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+RANDOM_STATE = 42  # The answer to life, the universe, and consistent results
